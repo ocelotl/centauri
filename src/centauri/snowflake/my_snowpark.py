4dd03554e6156afb2340bc2fa31179d6b4701546
Diego Hurtado <ocelotl@users.noreply.github.com>
2025-03-04 13:17:39 -0600

    Add snowflake to requirements

--- src/centauri/snowflake/my_snowpark.py
+++ src/centauri/snowflake/my_snowpark.py
@@ -1,6 +1,5 @@
 from ipdb import set_trace
 from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import col, udf
 from json import load
 
 with open("credentials.json") as credentials_file:
@@ -8,11 +7,9 @@ with open("credentials.json") as credentials_file:
 
 session = Session.builder.configs(credentials).create()
 
-@udf(name="capitalize", is_permanent=True, )
-
 print(session)
 
-table = session.table("FLOWER_DETAILS")
+table = session.table("INVENTORY")
 
 table.show()
 print(table.count())
